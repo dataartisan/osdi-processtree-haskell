befedbf789ef7ad8811bd9661a1d6e485133530a
Konstantin Läufer <laufer@cs.luc.edu>
2011-10-29 15:14:10 -0500

    simplified process map data structure

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@ import Data.IntMap ((!))
 import Data.Maybe
 
 data Proc = Proc(Int, Int, String)
-getCmd (Proc(_, _, cmd)) = cmd
     
 parser :: String -> String -> Proc
 parser header = 
@@ -19,10 +18,10 @@ parser header =
 printProcessTree :: [String] -> [String]
 printProcessTree (header : lines) = 
   let procs = map (parser header) lines
-      pmap = IntMap.fromList $ map (\p @ (Proc(pid, _, _)) -> (pid, p)) procs
-      tmap = IntMap.fromListWith (++) $ map (\p @ (Proc(pid, ppid, _)) -> (ppid, [pid])) procs
+      pmap = IntMap.fromList $ map (\(Proc(pid, _, cmd)) -> (pid, cmd)) procs
+      tmap = IntMap.fromListWith (++) $ map (\(Proc(pid, ppid, _)) -> (ppid, [pid])) procs
       showTrees l i = concatMap (showTree l) (tmap ! i)
-      showTree l i = ((concat $ replicate l " ") ++ (show i) ++ ": " ++ (getCmd $ pmap ! i)) :
+      showTree l i = ((concat $ replicate l " ") ++ (show i) ++ ": " ++ (pmap ! i)) :
                      if IntMap.member i tmap then showTrees (l + 1) i else []
   in
     showTrees 0 0
