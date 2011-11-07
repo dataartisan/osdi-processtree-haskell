550cdb59c984865ad15ca212359d576c7171e4c5
Konstantin Läufer <laufer@cs.luc.edu>
2011-11-06 20:59:54 -0600

    minor simplifications

--- Main.hs
+++ Main.hs
@@ -3,25 +3,26 @@ import qualified Data.IntMap as IntMap
 import Data.IntMap ((!))
 import Data.Maybe
 
-data Proc = Proc(Int, Int, String)
+type Proc = (Int, Int, String)
     
-parser :: String -> String -> Proc
+parser :: String -> String -> (Int, Proc)
 parser header = 
   let cols = words header 
       iPid = fromJust $ elemIndex "PID" cols
       iPpid = fromJust $ elemIndex "PPID" cols
       iCmd = fromJust $ findIndex (flip elem ["CMD", "COMMAND"]) cols
   in
-    \line -> let ws = words line in
-      Proc(read $ ws !! iPid, read $ ws !! iPpid, concat $ drop iCmd ws)
+    \line -> let ws = words line ; pid = read $ ws !! iPid in
+               (pid, (pid, read $ ws !! iPpid, concat $ drop iCmd ws))
 
 printProcessTree :: [String] -> [String]
 printProcessTree (header : lines) = 
-  let procs = map (parser header) lines
-      pmap = IntMap.fromList $ map (\(Proc(pid, _, cmd)) -> (pid, cmd)) procs
-      tmap = IntMap.fromListWith (++) $ map (\(Proc(pid, ppid, _)) -> (ppid, [pid])) procs
-      showTrees l i = concatMap (showTree l) (tmap ! i)
-      showTree l i = ((concat $ replicate l " ") ++ (show i) ++ ": " ++ (pmap ! i)) :
+  let 
+    ps = map (parser header) lines
+    pmap = IntMap.fromList ps
+    tmap = IntMap.fromListWith (++) $ map (\(_, (pid, ppid, _)) -> (ppid, [pid])) ps
+    showTrees l i = concatMap (showTree l) (tmap ! i)
+    showTree l i = ((concat $ replicate l " ") ++ (show i) ++ ": " ++ (case pmap ! i of (_, _, cmd) -> cmd)) :
                      if IntMap.member i tmap then showTrees (l + 1) i else []
   in
     showTrees 0 0
